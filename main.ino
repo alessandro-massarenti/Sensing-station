abf2e01d179942ae67882e65e77a66a590b8679f
Alessandro Massarenti <48263116+alessandro-massarenti@users.noreply.github.com>
2019-07-01 12:58:18 +0200

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,18 @@
+#include "DHT.h"
+#define DHTPIN 2
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+void setup(){
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop(){
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  Serial.print(h);
+  Serial.print(",");
+  Serial.println(t);
+  delay(5000);
+}
