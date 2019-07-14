e34ea012e28972760d4dc5a1197ee59abbe640b1
Alessandro Massarenti <48263116+alessandro-massarenti@users.noreply.github.com>
2019-07-14 22:21:03 +0200

    Updated main

--- linux-data_manipulator-server/main.cpp
+++ linux-data_manipulator-server/main.cpp
@@ -1,10 +1,35 @@
-#include <iostream>
-#include <unistd.h>
+#import <iostream>
 
 using namespace std;
 
+struct nodo {
+    int info;
+    nodo *next;
+
+    nodo(int a = 0, nodo *b = 0) {
+        info = a;
+        next = b;
+    }
+};
+
+void stampa_n(nodo *list) {
+    if (!list) {
+        cout << endl;
+        return;
+    }
+    cout << list->info << ' ';
+    stampa_n(list->next);
+}
+
 int main() {
-    int data[] = {10, 5, 13};  //Random data we want to send
+
+
+    return 0;
+}
+
+
+/*
+ int data[] = {10, 5, 13};  //Random data we want to send
     FILE *file;
     file = fopen("/dev/ttyUSB0", "w");  //Opening device file
     int i = 0;
@@ -14,4 +39,4 @@ int main() {
         sleep(1);
     }
     fclose(file);
-}
+ */
