daa4cd99dee445bcbc668d20ade77bd29cba8426
Alessandro Massarenti <48263116+alessandro-massarenti@users.noreply.github.com>
2020-01-28 00:13:32 +0100

    Mooved to ethernet communication
    Mooved to ethernet communication

--- arduino/sensor.ino
+++ arduino/sensor.ino
@@ -4,58 +4,81 @@
   È stato costruito da Alessandro Massarenti
   V 2.0
 */
+#include <DHT.h>
+#include <UIPEthernet.h> // Used for Ethernet
 
-#include "DHT.h"
+String readString = ""; // a String to hold incoming data
+int interval = 5000;
+unsigned long time, pvtime;
+float temp, humid;
+// **** ETHERNET SETTING ****
+byte mac[] = {0x90, 0xA2, 0xDA, 0x0D, 0x78, 0xEE};
+IPAddress ip(192, 168, 51, 55);
 
-String inputString = "";     // a String to hold incoming data
-bool stringComplete = false; // whether the String is complete
-DHT sensore1(2, 22);         // Sensor1 (air temp and humid)
+DHT sensore(3, 22);        // Sensor obj
+EthernetServer server(80); // Ethernet obj
 
 void setup()
 {
+  pvtime = millis();
+  readString.reserve(100);
+
+  sensore.begin();
+
+  Ethernet.begin(mac, ip); // start the Ethernet connection and the server:
+  server.begin();
   Serial.begin(9600);
-  inputString.reserve(200);
-  sensore1.begin();
 }
 
 void loop()
 {
-
-  if (stringComplete)
+  // Create a client connection
+  EthernetClient client = server.available();
+  if (client)
   {
-    if (inputString == "temp")
+    while (client.connected())
     {
-      Serial.print("temp");
-      Serial.println(sensore1.readTemperature());
+      if (client.available())
+      {
+        char c = client.read();
+
+        //read char by char HTTP request
+        if (readString.length() < 100)
+        {
+          //store characters to string
+          readString += c;
+        }
+
+        //if HTTP request has ended– 0x0D is Carriage Return \n ASCII
+        if (c == 0x0D)
+        {
+          if (millis() - pvtime >= interval)
+          {
+            // save the last time you blinked the LED
+            pvtime = millis();
+
+            temp = sensore.readTemperature();
+            humid = sensore.readHumidity();
+            Serial.println(temp);
+            Serial.println(humid);
+          }
+          client.println("HTTP/1.1 200 OK"); //send new page
+          client.println("Content-Type: application/json");
+          client.println();
+          
+          client.println("{\"data\": {\"temperature\":" + String(temp) + ",\"humidity\":" + String(humid) + "}}");
+
+          delay(10);
+          //stopping client
+          client.stop();
+
+          //clearing string for next read
+          readString = "";
+        }
+      }
     }
-    else if (inputString == "humi")
-    {
-      Serial.print("humi");
-      Serial.println(sensore1.readHumidity());
-    }
-    else
-    {
-      Serial.println(inputString);
-    }
-    // clear the String
-    inputString = "";
-    stringComplete = false;
   }
 
   // Clock
-  delay(10);
-}
-
-void serialEvent()
-{
-  while (Serial.available())
-  {
-    char inChar = (char)Serial.read(); // get the new byte:
-    // if the incoming character is a newline, set a flag so the main loop can
-    // do something about it:
-    if (inChar == '\n')
-      stringComplete = true;
-    else
-      inputString += inChar; // add it to the inputString:
-  }
+  delay(20);
 }
