c1d19434f4de89aede21534314e10ce5db2658e6
Alessandro Massarenti <48263116+alessandro-massarenti@users.noreply.github.com>
2020-03-05 00:30:31 +0100

    mooved to MQTT client type communication

--- arduino/sensor.ino
+++ arduino/sensor.ino
@@ -4,81 +4,81 @@
   È stato costruito da Alessandro Massarenti
   V 2.0
 */
+#define AIR_TEMP "/home/greenhouse/airTemp"
+#define AIR_HUMID "/home/greenhouse/airHumid"
+#define SOIL_HUMID "/home/greenhouse/soilHumid"
+#define ID_CONTROLLER "sensor_1"
+#define USERNAME "username"
+#define PASSWORD "password"
+#define PAUSE_T 10000
+
 #include <DHT.h>
 #include <UIPEthernet.h> // Used for Ethernet
+#include <PubSubClient.h>
 
-String readString = ""; // a String to hold incoming data
-int interval = 5000;
-unsigned long time, pvtime;
-float temp, humid;
-// **** ETHERNET SETTING ****
 byte mac[] = {0x90, 0xA2, 0xDA, 0x0D, 0x78, 0xEE};
-IPAddress ip(192, 168, 51, 55);
+IPAddress server(192, 168, 51, 117);
 
-DHT sensore(3, 22);        // Sensor obj
-EthernetServer server(80); // Ethernet obj
+DHT sensore(3, 22); // Sensor obj
+int soilSensor = A0;
+
+EthernetClient ethClient;
+PubSubClient mqttClient(ethClient);
+
+void reconnect()
+{
+  // Loop until we're reconnected
+  while (!mqttClient.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (mqttClient.connect(ID_CONTROLLER, USERNAME, PASSWORD))
+    {
+      Serial.println("connected");
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
 
 void setup()
 {
-  pvtime = millis();
-  readString.reserve(100);
-
-  sensore.begin();
-
-  Ethernet.begin(mac, ip); // start the Ethernet connection and the server:
-  server.begin();
   Serial.begin(9600);
+
+  mqttClient.setServer(server, 1883);
+
+  Ethernet.begin(mac);
+  sensore.begin();
+  // Allow the hardware to sort itself out
+  delay(500);
 }
 
 void loop()
 {
-  // Create a client connection
-  EthernetClient client = server.available();
-  if (client)
-  {
-    while (client.connected())
+  float airTemp, airHumid, soilHumid;
+
+  while(true){
+    if (!mqttClient.connected())
     {
-      if (client.available())
-      {
-        char c = client.read();
-
-        //read char by char HTTP request
-        if (readString.length() < 100)
-        {
-          //store characters to string
-          readString += c;
-        }
-
-        //if HTTP request has ended– 0x0D is Carriage Return \n ASCII
-        if (c == 0x0D)
-        {
-          if (millis() - pvtime >= interval)
-          {
-            // save the last time you blinked the LED
-            pvtime = millis();
-
-            temp = sensore.readTemperature();
-            humid = sensore.readHumidity();
-            Serial.println(temp);
-            Serial.println(humid);
-          }
-          client.println("HTTP/1.1 200 OK"); //send new page
-          client.println("Content-Type: application/json");
-          client.println();
-          
-          client.println("{\"data\": {\"temperature\":" + String(temp) + ",\"humidity\":" + String(humid) + "}}");
-
-          delay(10);
-          //stopping client
-          client.stop();
-
-          //clearing string for next read
-          readString = "";
-        }
-      }
+      reconnect();
     }
-  }
 
-  // Clock
-  delay(20);
+    soilHumid = analogRead(soilSensor);
+    airTemp = sensore.readTemperature();
+    airHumid = sensore.readHumidity();
+
+    Serial.println(airTemp);
+    Serial.println(airHumid);
+    Serial.println(String(airTemp).c_str());
+    mqttClient.publish(AIR_TEMP,  String(airTemp).c_str()  ,true);
+    mqttClient.publish(AIR_HUMID, String(airHumid).c_str()  ,true);
+    mqttClient.publish(SOIL_HUMID, String(soilHumid).c_str() ,true);
+    delay(2000);
+  }
 }
